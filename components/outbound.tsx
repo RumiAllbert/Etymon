ed1178d47213f1b545bb61938c557221c69c862c
Rumi Allbert <rumiallbert@gmail.com>
2025-02-23 16:55:45 +0100

    Remove GitHub link from outbound navigation

--- components/outbound.tsx
+++ components/outbound.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Coffee, Github, Home } from "lucide-react";
+import { Coffee, Home } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 
@@ -104,13 +104,6 @@ export default function Outbound() {
         className="dark:text-gray-400 text-gray-600 hover:dark:text-gray-100 hover:text-gray-900 transition-colors"
       >
         <Home className="w-5 h-5" />
-      </Link>
-      <Link
-        href="https://github.com/rumiallbert/etymon"
-        target="_blank"
-        className="dark:text-gray-400 text-gray-600 hover:dark:text-gray-100 hover:text-gray-900 transition-colors"
-      >
-        <Github className="w-5 h-5" />
       </Link>
       <CoffeeButton />
     </div>
